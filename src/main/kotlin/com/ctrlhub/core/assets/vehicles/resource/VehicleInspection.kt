052d1ab3cfac64d86b74bc2f52e4996a12afcb49
Jonathan Wilkinson <jonny@ctrl-hub.com>
2025-02-24 20:51:38 +0000

    task: add support for vehicle inspection comments

--- src/main/kotlin/com/ctrlhub/core/assets/vehicles/resource/VehicleInspection.kt
+++ src/main/kotlin/com/ctrlhub/core/assets/vehicles/resource/VehicleInspection.kt
@@ -1,5 +1,6 @@
 package com.ctrlhub.core.assets.vehicles.resource
 
+import com.ctrlhub.core.assets.vehicles.resource.VehicleInspectionChecks
 import com.ctrlhub.core.serializer.JacksonLocalDateTimeSerializer
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
@@ -16,12 +17,59 @@ import java.time.LocalDateTime
 class VehicleInspection @JsonCreator constructor(
     @JsonProperty("id") @Id(StringIdHandler::class) var id: String = "",
     @JsonProperty("checks") var checks: VehicleInspectionChecks? = null,
+    @JsonProperty("comments") var comments: VehicleInspectionComments? = null,
     @SerialName("inspected_at") @JsonProperty("inspected_at") var inspectedAt: LocalDateTime? = null
 ) {
     constructor() : this(
         id = "",
         checks = null,
+        comments = null,
         inspectedAt = null
+    )
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+class VehicleInspectionComments @JsonCreator constructor(
+    @JsonProperty("visible_damage") var visibleDamage: String? = null,
+    @JsonProperty("tyres") var tyres: String? = null,
+    @JsonProperty("washers_and_wipers") var washersAndWipers: String? = null,
+    @JsonProperty("windscreen") var windscreen: String? = null,
+    @JsonProperty("number_plate") var numberPlate: String? = null,
+    @JsonProperty("security") var security: String? = null,
+    @JsonProperty("accessories") var accessories: String? = null,
+    @JsonProperty("spare_number_plate") var spareNumberPlate: String? = null,
+    @JsonProperty("safe_access") var safeAccess: String? = null,
+    @JsonProperty("reversing_alarm") var reversingAlarm: String? = null,
+    @JsonProperty("beacons") var beacons: String? = null,
+    @JsonProperty("chemicals_and_fuel") var chemicalsAndFuel: String? = null,
+    @JsonProperty("storage") var storage: String? = null,
+    @JsonProperty("lights_and_indicators") var lightsAndIndicators: String? = null,
+    @JsonProperty("engine_warning_lights") var engineWarningLights: String? = null,
+    @JsonProperty("servicing") var servicing: String? = null,
+    @JsonProperty("levels") var levels: String? = null,
+    @JsonProperty("cleanliness") var cleanliness: String? = null,
+    @JsonProperty("driver_checks") var driverChecks: String? = null
+) {
+    constructor() : this(
+        visibleDamage = null,
+        tyres = null,
+        washersAndWipers = null,
+        windscreen = null,
+        numberPlate = null,
+        security = null,
+        accessories = null,
+        spareNumberPlate = null,
+        safeAccess = null,
+        reversingAlarm = null,
+        beacons = null,
+        chemicalsAndFuel = null,
+        storage = null,
+        lightsAndIndicators = null,
+        engineWarningLights = null,
+        servicing = null,
+        levels = null,
+        cleanliness = null,
+        driverChecks = null
     )
 }
 
