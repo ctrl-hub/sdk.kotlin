2ef49bffbc5fde6285207a6c0a6b12a6dc3faa60
Jonathan Wilkinson <jonny@ctrl-hub.com>
2025-03-14 11:54:39 +0000

    feat: support schemes and implement a router for it

---
+++ src/main/kotlin/com/ctrlhub/core/governance/schemes/SchemesRouter.kt
@@ -0,0 +1,56 @@
+package com.ctrlhub.core.governance.schemes
+
+import com.ctrlhub.core.Api
+import com.ctrlhub.core.governance.schemes.response.Scheme
+import com.ctrlhub.core.governance.schemes.workorders.response.WorkOrder
+import com.ctrlhub.core.router.Router
+import com.ctrlhub.core.router.request.FilterOption
+import com.ctrlhub.core.router.request.JsonApiIncludes
+import com.ctrlhub.core.router.request.RequestParametersWithIncludes
+import io.ktor.client.HttpClient
+
+enum class SchemeIncludes(val value: String): JsonApiIncludes {
+    WorkOrders("work_orders"),
+    WorkOrdersOperations("work_orders.operations");
+
+    override fun value(): String {
+        return value
+    }
+}
+
+class SchemeRequestParameters(
+    filterOptions: List<FilterOption> = emptyList(),
+    includes: List<SchemeIncludes> = emptyList()
+) : RequestParametersWithIncludes<SchemeIncludes>(filterOptions, includes)
+
+class SchemesRouter(httpClient: HttpClient) : Router(httpClient) {
+
+    /**
+     * Retrieve a list of all schemes
+     *
+     * @param organisationId String The organisation ID to retrieve all equipment items for
+     * @param requestParameters RequestParameters An instance of SchemeRequestParameters, capturing sorting, filtering and pagination based request params
+     *
+     * @return A list of all schemes
+     */
+    suspend fun all(organisationId: String, requestParameters: SchemeRequestParameters = SchemeRequestParameters()): List<Scheme> {
+        return fetchJsonApiResources("/v3/orgs/$organisationId/governance/schemes", requestParameters.toMap(), Scheme::class.java,
+            WorkOrder::class.java)
+    }
+
+    /**
+     * Retrieve a single scheme item
+     *
+     * @param organisationId String The organisation ID to retrieve all equipment items for
+     * @param schemeId String The scheme ID to retrieve data for
+     * @param requestParameters RequestParameters An instance of SchemeRequestParameters, capturing sorting, filtering and pagination based request params
+     *
+     * @return A single scheme
+     */
+    suspend fun one(organisationId: String, schemeId: String, requestParameters: SchemeRequestParameters = SchemeRequestParameters()) : Scheme {
+        return fetchJsonApiResource("/v3/orgs/$organisationId/governance/schemes/$schemeId", requestParameters.toMap())
+    }
+}
+
+val Api.schemes: SchemesRouter
+    get() = SchemesRouter(httpClient)
--- src/main/kotlin/com/ctrlhub/core/governance/schemes/workorders/operations/response/Operation.kt
+++ src/main/kotlin/com/ctrlhub/core/governance/schemes/workorders/operations/response/Operation.kt
@@ -4,6 +4,8 @@ import com.ctrlhub.core.iam.response.User
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.github.jasminb.jsonapi.StringIdHandler
+import com.github.jasminb.jsonapi.annotations.Id
 import com.github.jasminb.jsonapi.annotations.Relationship
 import com.github.jasminb.jsonapi.annotations.Type
 import java.time.LocalDate
@@ -11,13 +13,14 @@ import java.time.LocalDate
 @Type("operations")
 @JsonIgnoreProperties(ignoreUnknown = true)
 class Operation @JsonCreator constructor(
+    @Id(StringIdHandler::class) var id: String = "",
     @JsonProperty("name") var name: String = "",
     @JsonProperty("code") var code: String = "",
-    @JsonProperty("description") var description: String = "",
+    @JsonProperty("description") var description: String? = "",
     @JsonProperty("start_date") var startDate: LocalDate? = null,
     @JsonProperty("end_date") var endDate: LocalDate? = null,
-    @JsonProperty("uprns") var uprns: List<String> = emptyList(),
-    @JsonProperty("usrns") var usrns: List<String> = emptyList(),
+    @JsonProperty("uprns") var uprns: List<String>? = emptyList(),
+    @JsonProperty("usrns") var usrns: List<String>? = emptyList(),
     @JsonProperty("completed") var completed: Boolean = false,
     @JsonProperty("aborted") var aborted: Boolean = false,
     @JsonProperty("cancelled") var cancelled: Boolean = false,
