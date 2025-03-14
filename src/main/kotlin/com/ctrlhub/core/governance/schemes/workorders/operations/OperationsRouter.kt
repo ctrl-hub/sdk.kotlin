6a29384fa8da8a351e50cc8ce57b1ba3a66a72ea
Jonathan Wilkinson <jonny@ctrl-hub.com>
2025-03-14 12:46:38 +0000

    feat: add operations router

--- src/main/kotlin/com/ctrlhub/core/governance/schemes/workorders/operations/OperationsRouter.kt
+++ src/main/kotlin/com/ctrlhub/core/governance/schemes/workorders/operations/OperationsRouter.kt
@@ -1,19 +1,47 @@
 package com.ctrlhub.core.governance.schemes.workorders.operations
 
+import com.ctrlhub.core.Api
+import com.ctrlhub.core.governance.schemes.workorders.WorkOrdersRouter
 import com.ctrlhub.core.governance.schemes.workorders.operations.response.Operation
 import com.ctrlhub.core.router.Router
 import io.ktor.client.HttpClient
 
 class OperationsRouter(httpClient: HttpClient) : Router(httpClient) {
+
+    /**
+     * Retrieve a list of all operations
+     *
+     * @param organisationId String The organisation ID to retrieve all operations for
+     * @param schemeId String The scheme ID to retrieve all operations for
+     * @param workOrderId String The work order ID to retreive all operations for
+     *
+     * @return A list of all operations
+     */
     suspend fun all(organisationId: String, schemeId: String, workOrderId: String): List<Operation> {
         val endpoint = "/v3/orgs/$organisationId/governance/schemes/$schemeId/work-orders/$workOrderId/operations"
 
         return fetchJsonApiResources(endpoint)
     }
 
+    /**
+     * Retrieve a single operation
+     *
+     * @param organisationId String The organisation ID to retrieve all operations for
+     * @param schemeId String The scheme ID to retrieve all operations for
+     * @param workOrderId String The work order ID to retrieve all operations for
+     * @param operationId String The operation ID to retrieve data for
+     *
+     * @return A list of all operations
+     */
     suspend fun one(organisationId: String, schemeId: String, workOrderId: String, operationId: String): Operation {
         val endpoint = "/v3/orgs/$organisationId/governance/schemes/$schemeId/work-orders/$workOrderId/operations/$operationId"
 
         return fetchJsonApiResource(endpoint)
     }
 }
+
+val Api.operations: OperationsRouter
+    get() = OperationsRouter(httpClient)
+
+val WorkOrdersRouter.operations: OperationsRouter
+    get() = OperationsRouter(httpClient)
---
+++ src/test/kotlin/com/ctrlhub/core/governance/schemes/workorders/operations/OperationsRouterTest.kt
@@ -0,0 +1,70 @@
+package com.ctrlhub.core.governance.schemes.workorders.operations
+
+import com.ctrlhub.core.configureForTest
+import com.ctrlhub.core.governance.schemes.workorders.operations.response.Operation
+import io.ktor.client.*
+import io.ktor.client.engine.mock.*
+import io.ktor.http.*
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.test.assertIs
+import kotlin.test.assertNotNull
+
+class OperationsRouterTest {
+    @Test
+    fun `can retrieve all operations for work order`() {
+        val jsonFilePath = Paths.get("src/test/resources/governance/schemes/workorders/operations/all-operations-response.json")
+        val jsonContent = Files.readString(jsonFilePath)
+
+        val mockEngine = MockEngine { request ->
+            respond(
+                content = jsonContent,
+                status = HttpStatusCode.OK,
+                headers = headersOf(HttpHeaders.ContentType, "application/json")
+            )
+        }
+
+        val operationsRouter = OperationsRouter(httpClient = HttpClient(mockEngine).configureForTest())
+
+        runBlocking {
+            val response = operationsRouter.all(
+                organisationId = "123",
+                schemeId = "abc",
+                workOrderId = "def"
+            )
+
+            assertIs<List<Operation>>(response)
+            assertNotNull(response[0].id)
+        }
+    }
+
+    @Test
+    fun `can retrieve a single operation`() {
+        val jsonFilePath = Paths.get("src/test/resources/governance/schemes/workorders/operations/one-operation-response.json")
+        val jsonContent = Files.readString(jsonFilePath)
+
+        val mockEngine = MockEngine { request ->
+            respond(
+                content = jsonContent,
+                status = HttpStatusCode.OK,
+                headers = headersOf(HttpHeaders.ContentType, "application/json")
+            )
+        }
+
+        val operationsRouter = OperationsRouter(httpClient = HttpClient(mockEngine).configureForTest())
+
+        runBlocking {
+            val response = operationsRouter.one(
+                organisationId = "123",
+                schemeId = "abc",
+                workOrderId = "def",
+                operationId = "ghi"
+            )
+
+            assertIs<Operation>(response)
+            assertNotNull(response.id)
+        }
+    }
+}
