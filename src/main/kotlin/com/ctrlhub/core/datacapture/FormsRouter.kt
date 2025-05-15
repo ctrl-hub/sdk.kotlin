64d3ce8fe12ce9df5694df13611781989e58c4fb
Jonathan Wilkinson <jonny@ctrl-hub.com>
2025-05-15 14:47:21 +0100

    feat: support data capture forms endpoint

---
+++ src/main/kotlin/com/ctrlhub/core/datacapture/FormsRouter.kt
@@ -0,0 +1,19 @@
+package com.ctrlhub.core.datacapture
+
+import com.ctrlhub.core.Api
+import com.ctrlhub.core.datacapture.response.Form
+import com.ctrlhub.core.router.Router
+import io.ktor.client.HttpClient
+
+class FormsRouter(httpClient: HttpClient) : Router(httpClient) {
+    suspend fun all(organisationId: String): List<Form> {
+        return fetchJsonApiResources("/v3/orgs/${organisationId}/data-capture/forms", emptyMap(), Form::class.java)
+    }
+
+    suspend fun one(organisationId: String, formId: String): Form {
+        return fetchJsonApiResource("/v3/orgs/${organisationId}/data-capture-forms/${formId}")
+    }
+}
+
+val Api.forms: FormsRouter
+    get() = FormsRouter(httpClient)
---
+++ src/main/kotlin/com/ctrlhub/core/datacapture/response/Form.kt
@@ -0,0 +1,32 @@
+package com.ctrlhub.core.datacapture.response
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.github.jasminb.jsonapi.StringIdHandler
+import com.github.jasminb.jsonapi.annotations.Id
+import com.github.jasminb.jsonapi.annotations.Meta
+import com.github.jasminb.jsonapi.annotations.Type
+import java.time.LocalDateTime
+
+@Type("forms")
+@JsonIgnoreProperties(ignoreUnknown = true)
+class Form @JsonCreator constructor(
+    @Id(StringIdHandler::class) var id: String = "",
+    @JsonProperty("name") var name: String = "",
+    @JsonProperty("status") var status: String = "",
+    @Meta
+    var meta: FormMeta? = null
+) {
+    constructor(): this(
+        id = "",
+        name = "",
+        status = ""
+    )
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+class FormMeta @JsonCreator constructor(
+    @JsonProperty("created_at") var createdAt: LocalDateTime,
+    @JsonProperty("updated_at") var updatedAt: LocalDateTime
+)
