e0218e09f63d66f18e624a048fc41485de294ce8
Jonathan Wilkinson <jonny@ctrl-hub.com>
2025-06-05 10:45:26 +0100

    feat: support form schema meta

--- src/main/kotlin/com/ctrlhub/core/datacapture/response/FormSchema.kt
+++ src/main/kotlin/com/ctrlhub/core/datacapture/response/FormSchema.kt
@@ -1,9 +1,22 @@
 package com.ctrlhub.core.datacapture.response
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.github.jasminb.jsonapi.annotations.Meta
 import com.github.jasminb.jsonapi.annotations.Type
+import java.time.LocalDateTime
 
 @Type("form-schemas")
-data class FormSchema (
+data class FormSchema(
     val id: String? = null,
     val rawSchema: String? = null,
+    @Meta
+    var meta: FormSchemaMeta? = null,
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class FormSchemaMeta(
+    @JsonProperty("created_at") var createdAt: LocalDateTime,
+    @JsonProperty("updated_at") var updatedAt: LocalDateTime,
+    @JsonProperty("latest") var latest: String,
 )
