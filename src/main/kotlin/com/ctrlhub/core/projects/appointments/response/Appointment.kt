32b33614404c36d7767478478683e9df7ba93061
Jonathan Wilkinson <jonny@ctrl-hub.com>
2025-08-14 14:40:10 +0100

    Add Appointment model and integrate with Operation

---
+++ src/main/kotlin/com/ctrlhub/core/projects/appointments/response/Appointment.kt
@@ -0,0 +1,32 @@
+package com.ctrlhub.core.projects.appointments.response
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.github.jasminb.jsonapi.StringIdHandler
+import com.github.jasminb.jsonapi.annotations.Id
+import com.github.jasminb.jsonapi.annotations.Type
+
+@Type("appointments")
+@JsonIgnoreProperties(ignoreUnknown = true)
+class Appointment @JsonCreator constructor(
+    @Id(StringIdHandler::class) var id: String = "",
+    @JsonProperty("animals") val animals: Boolean = false,
+    @JsonProperty("end_time") val endTime: String = "",
+    @JsonProperty("medical_dependency") val medicalDependency: Boolean = false,
+    @JsonProperty("notes") val notes: String = "",
+    @JsonProperty("on_ecr") val onEcr: Boolean = false,
+    @JsonProperty("on_ecr_notes") val onEcrNotes: String = "",
+    @JsonProperty("start_time") val startTime: String = ""
+) {
+    constructor() : this(
+        id = "",
+        animals = false,
+        endTime = "",
+        medicalDependency = false,
+        notes = "",
+        onEcr = false,
+        onEcrNotes = "",
+        startTime = ""
+    )
+}
